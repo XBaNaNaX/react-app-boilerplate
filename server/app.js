decf8cdd2795e13d35da06c8c37f598be7de66fd
XBaNaNaX <y.chaimongkon@gmail.com>
2017-06-01 21:27:45 +0700

    Add api

--- server/app.js
+++ server/app.js
@@ -7,9 +7,57 @@ const favicon = require('serve-favicon');
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+var router = express.Router();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://admin:1q2w3e4r@ds161121.mlab.com:61121/inventory');
+var Unit = require('../model/units');
+router.route('/units')
+
+// create a unit (accessed at POST http://localhost:8080/api/units)
+    .post(function (req, res) {
+
+        var unit = new Unit();      // create a new instance of the Bear model
+        unit.name = req.body.name;  // set the units name (comes from the request)
+
+        // save the unit and check for errors
+        unit.save(function (err) {
+            if (err)
+                res.send(err);
+
+            res.json({message: 'Bear created!'});
+        });
+
+    })
+    .get(function (req, res) {
+        Unit.find(function (err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+router.use(function (req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to our api!'});
+});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use('/api', router);
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
-app.use(favicon(path.join(__dirname,'..', 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 import TextBox from './Components/Controls/TextBox';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <TextBox className={"txt1"} id={"txt1"} handleChange={(txt)=>{console.log(txt)}}/>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {units: null};
+    }
+
+    async loadUnit() {
+        let _this = this;
+        await axios.get('http://localhost:5000/api/units')
+            .then(function (response) {
+                console.log(response);
+                _this.setState({units: response.data});
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    async componentDidMount() {
+        this.loadUnit();
+    }
+
+    render() {
+        let {units} = this.state;
+        let _this = this;
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <div style={{
+                    display: 'flex',
+                    flex: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'center'
+                }}>
+
+                    <TextBox className={"txt1"} id={"txt1"} handleChange={(txt) => {
+                        console.log(txt.target.value)
+                    }}/>
+                    <button onClick={(e)=>{
+                        e.preventDefault()
+                        axios.post('http://localhost:5000/api/units', {
+                            name: document.getElementById("txt1").value
+                        })
+                            .then(function (response) {
+                                document.getElementById("txt1").value = "";
+                                console.log(response);
+                                _this.loadUnit();
+                            })
+                            .catch(function (error) {
+                                document.getElementById("txt1").value = "";
+                                console.log(error);
+                            });
+                    }}>Add Unit</button>
+                </div>
+                <div style={{
+                    height: '200px',
+                    overflowY: 'scroll',
+                    width: '50%',
+                    backgroundColor: 'antiquewhite',
+                    display: 'inline-block',
+                    marginTop:'20px'
+                }}>
+                {
+                    units !== null ?
+                        units.map((unit, index) => {
+                            return <div key={index}><h3>{unit._id}:{unit.name}</h3></div>
+                        })
+                    : <h1>Loading...</h1>
+                }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;

bda88089131f9c0d9deac9cf22010a529ba000a2
XBaNaNaX <y.chaimongkon@gmail.com>
2017-06-07 21:20:07 +0700

    Add api(5) get single unit. put delete

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ router.route('/units')
 // create a unit (accessed at POST http://localhost:8080/api/units)
     .post(function (req, res) {
 
-        var unit = new Unit();      // create a new instance of the Bear model
+        var unit = new Unit();      // create a new instance of the Unit model
         unit.name = req.body.name;  // set the units name (comes from the request)
 
         // save the unit and check for errors
@@ -28,16 +28,56 @@ router.route('/units')
             if (err)
                 res.send(err);
 
-            res.json({message: 'Bear created!'});
+            res.json({message: 'Units created!'});
         });
 
     })
     .get(function (req, res) {
-        Unit.find(function (err, bears) {
+        Unit.find(function (err, units) {
             if (err)
                 res.send(err);
 
-            res.json(bears);
+            res.json(units);
+        });
+    });
+
+    router.route('/units/:unit_id')
+
+    // get the unit with that id (accessed at GET http://localhost:8080/api/units/:unit_id)
+    .get(function(req, res) {
+        Unit.findById(req.params.unit_id, function(err, unit) {
+            if (err)
+                res.send(err);
+            res.json(unit);
+        });
+    })
+    .put(function(req, res) {
+
+        // use our unit model to find the unit we want
+        Unit.findById(req.params.unit_id, function(err, unit) {
+
+            if (err)
+                res.send(err);
+
+            unit.name = req.body.name;  // update the units info
+
+            // save the unit
+            unit.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Unit updated!' });
+            });
+
+        });
+    }).delete(function(req, res) {
+        Unit.remove({
+            _id: req.params.unit_id
+        }, function(err, unit) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
         });
     });
 

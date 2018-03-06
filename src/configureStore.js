8f7cb90e45d1badc7b4a9f9d0f88cda2ba291aa0
XBaNaNaX <y.chaimongkon@gmail.com>
2018-03-06 15:09:37 +0700

    commit redux saga

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,15 @@
 import { createStore,applyMiddleware } from 'redux'
 import app from './reducers'
-import thunk from 'redux-thunk';
+
+import createSagaMiddleware from 'redux-saga';
+import dataSaga from './saga';
+
+const sagaMiddleware = createSagaMiddleware()
 
 export default function configureStore() {
-  let store = createStore(app,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
+  let store = createStore(app,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(sagaMiddleware))
+    sagaMiddleware.run(dataSaga)
   return store
 }
